933aa98720872a3840b9e0f6e5d8d316e8d4eaee
Emersion <contact@emersion.fr>
2015-03-29 12:31:07 +0200

    Adds checks when writing to stream

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ servoblaster.createWriteStream = function (pin) {
 	var stream = fs.createWriteStream(SERVOBLASTER_DEV);
 
 	var servo = through(function write (data) {
+		if (!pin && pin !== 0) {
+			if (typeof data.pin != 'undefined') {
+				pin = data.pin;
+			}
+			if (typeof data.value != 'undefined') {
+				data = data.value;
+			}
+			if (!pin && pin !== 0) {
+				throw new Error('No pin specified');
+			}
+		}
+		if (!data && data !== 0) {
+			throw new Error('No value specified');
+		}
 		stream.write(pin+'='+data);
 	}, function end () {
 		stream.end();
